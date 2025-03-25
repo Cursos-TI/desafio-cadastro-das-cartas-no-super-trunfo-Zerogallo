7e13c2c30df6e5572379a143e2e132c1c975f870
Renan F <167368572+Zerogallo@users.noreply.github.com>
2025-03-25 11:40:04 +0000

    finalizado com sucesso

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,16 @@
 
 // Estrutura para armazenar as informações de uma cidade
 struct Cidade {
+    
     char nome[100];          // Nome da cidade
     int populacao;           // População
     float area;              // Área em km²
     float pib;               // PIB em bilhões de unidades monetárias
     int pontosTuristicos;    // Número de pontos turísticos
+};
+// Estrutura para armazenar informações de um país
+struct Pais {
+    char nomePais[100];      // Nome do país
 };
 
 // Função para exibir as informações de uma cidade
@@ -26,19 +31,25 @@ void exibirCidade(struct Cidade cidade, char estado, int cidadeNumero, char nome
 }
 
 int main() {
-    char nomePais[100]; // Nome do país
-    char estados[NUM_ESTADOS] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'}; // Estados de A a H
+   
+    struct Pais estados[NUM_ESTADOS]; // Estados de A a H
     struct Cidade cidades[NUM_ESTADOS][NUM_CIDADES]; // Matriz de cidades
 
-    // Perguntar o nome do país
-    printf("Digite o nome do pais: ");
-    scanf("%s", nomePais);  // Captura o nome do país
+  
 
     // Preenchendo os dados das cidades
     for (int i = 0; i < NUM_ESTADOS; i++) {
-        for (int j = 0; j < NUM_CIDADES; j++) {
-            printf("Cadastro da cidade %d do estado %c:\n", j + 1, estados[i]);
 
+         // Perguntar o nome do país
+         printf("Digite o nome do pais do estado %c: ", 'A' + i);
+         scanf("%s", estados[i].nomePais);   // Captura o nome do país
+
+        for (int j = 0; j < NUM_CIDADES; j++) {
+
+           
+
+            printf("Cadastro da cidade %d do estado %c:\n", j + 1, 'A' + i);
+            
             printf("Digite o nome da cidade: ");
             scanf("%s", cidades[i][j].nome);
 
@@ -62,7 +73,7 @@ int main() {
     printf("Cadastro das cidades:\n");
     for (int i = 0; i < NUM_ESTADOS; i++) {
         for (int j = 0; j < NUM_CIDADES; j++) {
-            exibirCidade(cidades[i][j], estados[i], j + 1, nomePais);  // Passa o nome do país
+            exibirCidade(cidades[i][j], 'A' + i, j + 1, estados[i].nomePais);   // Passa o nome do país
         }
     }
 
